5569fdcf5abf231ead29f33ce05522099a80b349
PaulaFarcas <paulafarcas2002@yahoo.com>
2023-04-18 08:17:34 +0300

    Updated version

--- project1.c
+++ project1.c
@@ -1,167 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[]){
-
-
-    if(argc < 2){
-        printf("not enough arguments! provide multiple arguments representing different paths\n");
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+ 
+int main(int argc,char* argv[]){
+   
+    if(argc<1){
+        printf("error");
         exit(EXIT_FAILURE);
     }
     else{
-        for(int i = 1; i < argc; i++){
+        for(int i=1;i<argc;i++){
+             struct stat buff;
+             lstat(argv[i],&buff);
+        if(S_ISREG(buff.st_mode)){
+            printf("regular file\n");
+            printf("Menu:\n");
+            printf("-n (file name)\n");
+            printf("-d (file dim/size)\n");
+            printf("-h (number of hard links)\n");
+            printf("-m (time of last modification)\n");
+            printf("-a (access rights)\n");
+            printf("-l (create a sym link give: link name)\n");
 
-            struct stat info;
-            lstat(argv[i], &info);
-
-            mode_t m;
-
-            m = info.st_mode;
-
-            if(S_ISREG(m)){
-                printf("argument %d is reg\n", i);
-
-                printf("\n");
-                printf("Menu for a regular file:\n");
-                printf("-n (file name)\n");
-                printf("-d (file dim/size)\n");
-                printf("-h (number of hard links)\n");
-                printf("-m (time of last modification)\n");
-                printf("-a (access rights)\n");
-                printf("-l (create a sym link give: link name)\n");
-
-                printf("\n");
-                printf("Choose one or more options\n");
-
-                char option[52];
-
-                scanf("%s", option);
-
-                printf("%s\n", option);
-
-                if(strstr(option, "n") || strstr(option, "-n")){
-                    printf("\nThe file name is %s\n", argv[i]);
-                }
-
-                if(strstr(option, "d") || strstr(option, "-d")){
-                    off_t size = info.st_size;
-                    printf("\nThe file dimension/size is %ld bytes\n", size);
-                }
-
-                if(strstr(option, "h") || strstr(option, "-h")){
-                    nlink_t noOfHardLinks = info.st_nlink;
-                    printf("\nThe number of hard links is %ld \n", noOfHardLinks);
-                }
-
-                if(strstr(option, "m") || strstr(option, "-m")){
-                    printf("\nTime of last modification is: %s", ctime(&info.st_mtime));
-                }
-                
-                if(strstr(option, "a") || strstr(option, "-a")){
-                    printf("\nAccess rights: \n");
-                    printf((S_ISDIR(info.st_mode)) ? "d" : "-");
-                    printf((info.st_mode & S_IRUSR) ? "r" : "-");
-                    printf((info.st_mode & S_IWUSR) ? "w" : "-");
-                    printf((info.st_mode & S_IXUSR) ? "x" : "-");
-                    printf((info.st_mode & S_IRGRP) ? "r" : "-");
-                    printf((info.st_mode & S_IWGRP) ? "w" : "-");
-                    printf((info.st_mode & S_IXGRP) ? "x" : "-");
-                    printf((info.st_mode & S_IROTH) ? "r" : "-");
-                    printf((info.st_mode & S_IWOTH) ? "w" : "-");
-                    printf((info.st_mode & S_IXOTH) ? "x" : "-");
-                    printf("\n\n");
-                }
-
-                if(strstr(option, "l") || strstr(option, "-l")){
-                    char linkName[52];
-                    printf("\nIntroduce the link name: ");
-                    scanf("%s", linkName);
-                    symlink(argv[i], linkName);
-                }
-                
+            char option[2];
+            scanf("%s",&option);
+            if(strcmp(option,"-n")==0){
+                printf("file name:%s \n",argv[i]);
             }
-            else if(S_ISDIR(m)){
-                printf("argument %d is dir\n", i);
+            if(strcmp(option,"-d")==0){
+                printf("size %s\n",buff.st_size);
             }
-            else if(S_ISLNK(m)){
-                printf("argument %d is link\n", i);
-
-                printf("\n");
-                printf("Menu for a symbolic link:\n");
-                printf("-n (link name)\n");
-                printf("-l (delete link)\n");
-                printf("-d (size of the link)\n");
-                printf("-t (size of the target)\n");
-                printf("-a (access rights)\n");
-
-                printf("\n");
-                printf("Choose one or more options (Note that if the -l or l option is given, the other following options won't be performed!!)\n");
-
-                char option[52];
-
-                scanf("%s", option);
-
-                printf("%s\n", option);
-
-                if(strstr(option, "n") || strstr(option, "-n")){
-                    printf("\nThe link name is %s\n", argv[i]);
-                }
-
-                if(strstr(option, "l") || strstr(option, "-l")){
-                    if(unlink(argv[i]) != 0){
-                        perror("unlink() error\n");
-                    }else{
-                        printf("The link %s was deleted\n", argv[i]);
-                        //exit(0);
-                    }                    
-                }
-
-                if(strstr(option, "d") || strstr(option, "-d")){
-                    off_t size = info.st_size;
-                    printf("\nThe size of the link is %ld bytes\n", size);
-                }
-
-                if(strstr(option, "t") || strstr(option, "-t")){
-                    char target_path[256];
-                    ssize_t len = readlink(argv[i], target_path, sizeof(target_path)-1);
-
-                    if (len != -1) {
-                        target_path[len] = '\0';
-                        if (lstat(target_path, &info) == 0) {
-                            if (S_ISREG(info.st_mode)) {
-                                printf("Target file size: %lld bytes\n", (long long) info.st_size);
-                            } else {
-                                printf("Target is not a regular file.\n");
-                            }
-                        } else {
-                            perror("lstat");
-                            exit(EXIT_FAILURE);
-                        }
-                    } else {
-                        perror("readlink");
-                        exit(EXIT_FAILURE);
-                    }
-                }
-                
-                if(strstr(option, "a") || strstr(option, "-a")){
-                    printf("\nAccess rights: \n");
-                    printf((S_ISDIR(info.st_mode)) ? "d" : "-");
-                    printf((info.st_mode & S_IRUSR) ? "r" : "-");
-                    printf((info.st_mode & S_IWUSR) ? "w" : "-");
-                    printf((info.st_mode & S_IXUSR) ? "x" : "-");
-                    printf((info.st_mode & S_IRGRP) ? "r" : "-");
-                    printf((info.st_mode & S_IWGRP) ? "w" : "-");
-                    printf((info.st_mode & S_IXGRP) ? "x" : "-");
-                    printf((info.st_mode & S_IROTH) ? "r" : "-");
-                    printf((info.st_mode & S_IWOTH) ? "w" : "-");
-                    printf((info.st_mode & S_IXOTH) ? "x" : "-");
-                    printf("\n\n");
-                }
+            if(strcmp(option,"-h")==0){
+                printf("nr hard links %s \n",buff.st_nlink);
             }
+            if(strcmp(option,"-m")==0){
+                 printf("time of last modify %s \n",buff.st_mtime);
+            }
+            if(strcmp(option,"-a")==0){
+                printf("acces right: %s");
+                if(S_IRUSR & st_mode) printf("-r");
+                if(S_IWUSR & st_mode) printf("-w");
+                if(S_IXUSR & st_mode) printf("-e");
+                if(S_IRGRP & st_mode) printf("-r");
+                if(S_IWGRP & st_mode) printf("-w");
+                if(S_IXGRP & st_mode) printf("-e");
+                if(S_IROTH & st_mode) printf("-r");
+                if(S_IWOTH & st_mode) printf("-w");
+                if(S_IROTH & st_mode) printf("-e");
+                else printf("-");
+            }
+            if(strcmp(option,"-l")==0){
+                printf("give link name %s \n");
+                char name[26];
+                scanf("%s",linkName);
+                pritnf("creat syn link %s \n",sym(argv[i],linkName));
+            }
+ 
+           
+           
+            
+           
+           
+           
+            //
+           
+            
+       }
+        else if(S_ISDIR(buff.st_mode)){
+            printf("directory\n");
+
+        }
+        else if(S_ISLNK(buff.st_mode)){
+            printf("simbolik link\n");
+        }
+        else{
+            printf("not regular,directory or simbolik link file\n");
+        }
+
         }
     }
+
+   
+
+    return 0;
 }
---
+++ project2.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <dirent.h>
+
+
+int main(int argc,char* argv[]){
+   
+    if(argc<1){
+        printf("error");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        for(int i=1;i<argc;i++){
+             struct stat buff;
+             lstat(argv[i],&buff);
+        if(S_ISREG(buff.st_mode)){
+            printf("regular file\n");
+            printf("Menu:\n");
+            printf("-n (file name)\n");
+            printf("-d (file dim/size)\n");
+            printf("-h (number of hard links)\n");
+            printf("-m (time of last modification)\n");
+            printf("-a (access rights)\n");
+            printf("-l (create a sym link give: link name)\n");
+
+            char option[2];
+            scanf("%s",option);
+            if(strcmp(option,"-n")==0){
+                printf("file name:%s \n",argv[i]);
+            }
+            if(strcmp(option,"-d")==0){
+                printf("size %ld\n",buff.st_size);
+            }
+            if(strcmp(option,"-h")==0){
+               printf("nr hard links %ld \n",buff.st_nlink);
+            }
+            if(strcmp(option,"-m")==0){
+               printf("time of last modify %ld \n",buff.st_mtime);
+            }
+            if(strcmp(option,"-a")==0){
+                printf("acces right: ");
+                if(S_IRUSR & buff.st_mode) printf("-r\n");
+                if(S_IWUSR & buff.st_mode) printf("-w\n");
+                if(S_IXUSR & buff.st_mode) printf("-e\n");
+                if(S_IRGRP & buff.st_mode) printf("-r\n");
+                if(S_IWGRP & buff.st_mode) printf("-w\n");
+                if(S_IXGRP & buff.st_mode) printf("-e\n");
+                if(S_IROTH & buff.st_mode) printf("-r\n");
+                if(S_IWOTH & buff.st_mode) printf("-w\n");
+                if(S_IROTH & buff.st_mode) printf("-e\n");
+                else printf("-");
+            }
+            if(strcmp(option,"-l")==0){
+                printf("give link name:\n");
+                char linkName[26];
+                scanf("%s",linkName);
+                symlink(argv[i],linkName);
+            }           
+            
+       }
+        else if(S_ISDIR(buff.st_mode)){
+            printf("directory\n");
+            printf("Menu: \n");
+            printf("-n name\n");
+            printf("-d size \n");
+            printf("-a acces right\n");
+            printf("-c total number of c files\n");
+            char option[2];
+            scanf("%s",option);
+            if(strcmp(option,"-n")==0){
+                printf("directorry name is %s\n",argv[i]);
+            }
+            if(strcmp(option,"-d")==0){
+                printf("size of directory is %ld \n",buff.st_size);
+            }
+            if(strcmp(option,"-a")==0){
+                printf("acces right: ");
+                if(S_IRUSR & buff.st_mode) printf("-r\n");
+                if(S_IWUSR & buff.st_mode) printf("-w\n");
+                if(S_IXUSR & buff.st_mode) printf("-e\n");
+                if(S_IRGRP & buff.st_mode) printf("-r\n");
+                if(S_IWGRP & buff.st_mode) printf("-w\n");
+                if(S_IXGRP & buff.st_mode) printf("-e\n");
+                if(S_IROTH & buff.st_mode) printf("-r\n");
+                if(S_IWOTH & buff.st_mode) printf("-w\n");
+                if(S_IROTH & buff.st_mode) printf("-e\n");
+                else printf("-");
+            }
+        
+            if(strcmp(option,"-c")==0){
+                struct dirent *directory;
+                DIR *dir;
+                dir=opendir(argv[i]);
+                while((directory=readdir(dir))!=NULL){
+                    char names[1024];
+                    snprintf(names,sizeof(names),"%s/%s",argv[i],directory->d_name);
+                }
+            }
+
+
+        }
+        else if(S_ISLNK(buff.st_mode)){
+            printf("simbolik link\n");
+            printf("Menu:\n");
+            printf("-n link name\n");
+            printf("-l delete link\n");
+            printf("-d size of a link\n");
+            printf("-t size of target\n");
+            printf("-a acces right for symbolic link\n");
+            char option[2];
+            scanf("%s",option);
+            if(strcmp(option,"-n")==0){
+                printf("link name is %s\n", argv[i]);
+            }
+            if(strcmp(option,"-l")==0){
+                unlink(argv[i]);
+                printf("link deleted\n");
+               
+            }
+            if(strcmp(option,"-d")==0){
+                printf("size of link is %ld\n",buff.st_size);
+            }
+            if(strcmp(option,"-t")==0){
+                struct stat target;
+                stat(argv[i],&target);
+                printf("size of target is: %ld\n",target.st_size);
+            }
+            if(strcmp(option,"-a")==0){
+                printf("acces right: ");
+                if(S_IRUSR & buff.st_mode) printf("-r\n");
+                if(S_IWUSR & buff.st_mode) printf("-w\n");
+                if(S_IXUSR & buff.st_mode) printf("-e\n");
+                if(S_IRGRP & buff.st_mode) printf("-r\n");
+                if(S_IWGRP & buff.st_mode) printf("-w\n");
+                if(S_IXGRP & buff.st_mode) printf("-e\n");
+                if(S_IROTH & buff.st_mode) printf("-r\n");
+                if(S_IWOTH & buff.st_mode) printf("-w\n");
+                if(S_IROTH & buff.st_mode) printf("-e\n");
+                else printf("-");
+            }
+       }
+        else{
+            printf("not regular,directory or simbolik link file\n");
+        }
+
+        }
+    }
+
+   
+
+    return 0;
+}
